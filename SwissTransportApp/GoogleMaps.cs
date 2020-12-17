af74992b3952e8bd98ba7c131c49408301c0d676
RobinSigrist <sigrist@simpit.ch>
2020-12-17 08:27:46 +0100

    mergin

---
+++ SwissTransportApp/GoogleMaps.cs
@@ -0,0 +1,26 @@
+﻿using SwissTransport.Core;
+using SwissTransport.Models;
+using System.Windows.Forms;
+
+namespace SwissTransportApp
+{
+    class GoogleMaps
+    {
+        static Transport transp = new Transport();
+
+        public static void Show(string target)
+        {
+            Stations stations = transp.GetStations(target);
+            if (stations.StationList.Count != 0)
+            {
+                // Get first entry of X and Y axis
+                GoogleMapsForm gmap = new GoogleMapsForm(
+                    stations.StationList[0].Coordinate.XCoordinate, 
+                    stations.StationList[0].Coordinate.YCoordinate
+                );
+                // show form with the Google Maps nuGet Package
+                gmap.Show();
+            }
+        }
+    }
+}
---
+++ SwissTransportApp/GoogleMapsForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace SwissTransportApp
+{
+    public partial class GoogleMapsForm : Form
+    {
+        double XCoordinate;
+        double YCoordinate;
+
+        public GoogleMapsForm(double XCoordinate, double YCoordinate)
+        {
+            InitializeComponent();
+            this.XCoordinate = XCoordinate;
+            this.YCoordinate = YCoordinate;
+        }
+
+        private void GoogleMapsForm_Load(object sender, EventArgs e)
+        {
+            gmapCtrl.DragButton = MouseButtons.Left;
+            gmapCtrl.CanDragMap = true;
+            gmapCtrl.MapProvider = GMapProviders.GoogleMap;
+            gmapCtrl.Position = new PointLatLng(XCoordinate, YCoordinate);
+            gmapCtrl.MinZoom = 0;
+            gmapCtrl.MaxZoom = 24;
+            gmapCtrl.Zoom = 9;
+            gmapCtrl.AutoScroll = true;
+            GMapOverlay markersOverlay = new GMapOverlay("marker1");
+            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(XCoordinate, YCoordinate), GMarkerGoogleType.red);
+            markersOverlay.Markers.Add(marker);
+            gmapCtrl.Overlays.Add(markersOverlay);
+        }
+    }
+}
